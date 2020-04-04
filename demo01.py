2b31a9adc9ed8fff408c3ed1cbea2a7cad1f597f
yangchen <yangchen9379@163.com>
2020-04-04 20:06:21 +0800

    第一次向GitHub提交数据

---
+++ demo01.py
@@ -0,0 +1,24 @@
+# print("hello world")
+# print("哈哈",123)
+# a = input("请输入信息1：")  # input()函数，默认输入的是字符串
+# b = input("请输入信息2：")
+# print("输入的内容是：",a+b)
+# print(type(12))
+# print(type(1.2))
+# print(type("yc"))
+# print(type("杨晨"))
+# print(type(True))
+# print(type( () ))   #元组
+# print(type(  [] ))  #数组
+# print(type(  {} ))  #字典
+# a = int(input("输入1："))
+# b = int(input("输入2："))
+# print("结果是：",a+b)
+# c = float(input())
+# d = float(input())
+# print("结果是：",c+d)
+
+a = input("请输入：")    # input()函数，默认输入的是字符串
+b = input("请输入：")
+c = len(a+b)
+print(c)
